229228b97c9b8d5551eae69b66f474cf1134ffe2
Roman Kyrylenko <pamuk635@gmail.com>
2018-10-16 13:09:20 +0300

    [Fix] warning

--- Example/StarWarsAnimations/Extension/UIColor+String.swift
+++ Example/StarWarsAnimations/Extension/UIColor+String.swift
@@ -15,7 +15,7 @@ extension UIColor {
         var int = UInt32()
         Scanner(string: hex).scanHexInt32(&int)
         let a, r, g, b: UInt32
-        switch hex.characters.count {
+        switch hex.count {
         case 3: // RGB (12-bit)
             (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
         case 6: // RGB (24-bit)
